52a75727365897812773bb9f2efdc0f7d3ff6064
Vladislav  Zinovyev <vladozdeveloper@gmail.com>
2022-12-17 23:40:37 +0300

    console log new posts

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
+import axios from 'axios';
 import React from 'react';
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
 
+const baseURL='https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty'
+
 function App() {
+
+  const [post, setPost] = React.useState([]);
+
+      useEffect(() => {
+        axios.get(baseURL).then((response) => {
+          setPost(response.data);
+        });
+      }, []);
+
+      if (!post) return null;
+
   return (
     <div className="App">
-      
+      {post.map(num=>
+        <p>{num}</p>
+      )}
     </div>
   );
 }
