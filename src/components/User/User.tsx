d4d527096b55793a58ab650f831637267dae6097
Vladislav  Zinovyev <vladozdeveloper@gmail.com>
2022-12-26 00:27:13 +0300

    completed component user

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -1,22 +1,37 @@
 import { Avatar, Button, Card,Stack, CardActions, CardContent, Typography } from "@mui/material";
 import axios from "axios";
-import { useState } from "react";
-import { NavLink, useNavigate, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, NavLink, useNavigate, useParams } from "react-router-dom";
 import logo from '../../image/logo.png'
-import { IUser } from "../../utils/types";
+import { converterDate } from "../../utils/converter";
+import { IComment, IUser } from "../../utils/types";
+import Comment from '../Comment/comment'
+import './User.css';
 
 const User=()=>{
     let params = useParams();
     let navigate = useNavigate();
 
+    const [loading, setLoading] = useState(false);
     const [user, setUser]=useState<IUser>();
     const [show,setShow]=useState(false);
+    const [comments,setComments]=useState<IComment[]>([]);
 
     async function fetchUser(){
-        await axios.get(`https://hacker-news.firebaseio.com/v0/item/${params.by}.json`)
+        await axios.get(`https://hacker-news.firebaseio.com/v0/user/${params.by}.json`)
         .then(async (response)=>{
             await setUser(response.data);
+            console.log(response.data)
         })
+    }
+    async function fetchComments(){
+        setComments([]);
+        user?.submitted?.map((c,i)=>(
+            axios.get(`https://hacker-news.firebaseio.com/v0/item/${c}.json`)
+            .then(async (response)=>{
+                await setComments(pos=>[...pos,response.data]);
+            })
+        ))  
     }
 
     const showComment=(e:any)=>{
@@ -30,6 +45,18 @@ const User=()=>{
     const refreshPage = ()=>{
         window.location.reload();
     }
+
+    useEffect(()=>{
+        fetchUser();
+        fetchComments();
+    },[params.by]);
+
+    useEffect(()=>{
+        setLoading(true);
+        fetchComments();
+        setLoading(false);
+    },[show]);
+
     return(
         
         <section className='user'>
@@ -41,8 +68,8 @@ const User=()=>{
                 
             </div>
 
-            <Card>
-            <CardContent>
+            <Card className="card">
+                <CardContent>
                     
                     <div className="flex">
                         <Stack direction="row" spacing={2}>
@@ -54,25 +81,42 @@ const User=()=>{
                         </Stack>
                         
                         <div className="infouser">
-                            <p className="username">
-                                <p style={{color: '#1976d2'}}>{params.by}</p>
-                                <NavLink style={{color: '#1976d2'}} to={`/userid/${postItem?.by}`}>
-                                    {postItem?.by}
-                                </NavLink>
-                            </p>
-                            <p className="date">{converterDate(postItem?.time)}</p>
+                            <p className="username">{params.by}</p>
                         </div>
                         
                     </div>
-                    <Typography variant="h5" >
-                        {postItem?.title}
-                    </Typography>
+                    <div className="aboutuser">
+                        
+                        <p>Creation date of the user, in Unix Time: {converterDate(user?.created)}</p>
+                        <p>The user's karma: {user?.karma}</p>
+                        {!user?.about ? <div>no Self-description of the user</div> : <div>The user's self-description:<a href={user?.about}>{user?.about}</a></div>}
+                        <p>{user?.submitted.length} stories, polls and comments by {params.by}</p>
+                        
+                    </div>
                 </CardContent>
 
-                <CardActions>
-                    <Button size="medium" onClick={(e)=>showComment(e)}>{} comments by {}</Button>
-                </CardActions>
+
             </Card>
+            {show && 
+                (loading ? (
+                    <Card className="block1">
+                        <div className="textcomment">Loading</div>
+                    </Card>
+                ):(
+                    !user?.submitted ? (
+                        <Card className="nocomment">
+                            <div className="textcomment">There are no comments</div>
+                        </Card> 
+                    ): (
+                        comments.map((c,i)=>(
+                        <div className="block1">
+                            <Comment by={c.by} id={c.id} parent={c.parent} kids={c?.kids} text={c.text} time={c.time} type={c.type}/>
+                        </div>
+                    )))
+                )
+                )
+                
+            }
         </section>
         
     )
