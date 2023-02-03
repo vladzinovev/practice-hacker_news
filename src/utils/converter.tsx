b1f1e7ffd6bbae786ece6bf2e9bc9f2e04646f0b
Vladislav  Zinovyev <vladozdeveloper@gmail.com>
2023-02-03 23:28:42 +0300

    hacker news 2.0

--- src/utils/converter.tsx
+++
@@ -1,8 +0,0 @@
-export function converterDate(times: number | null | undefined) {
-  if (!times) return "";
-
-  return new Date(times * 1000).toLocaleDateString("ru-RU", {
-    hour: "numeric",
-    minute: "numeric",
-  });
-}
